82b4491d80f87cf2719717c2771c28ab35304366
Jerry Hanks <jerryhanksokafor@gmail.com>
2019-08-05 22:05:01 +0100

    chore : add untracked files

---
+++ on-the-map/TabViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TabViewController.swift
+//  on-the-map
+//
+//  Created by Jerry Hanks on 03/08/2019.
+//  Copyright © 2019 Jerry. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
